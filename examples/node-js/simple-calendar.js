707eca251e72215c96d0d788deb10f068187ee80
Wesley de Souza <wesleywex@gmail.com>
2015-02-23 20:38:55 +0100

    Added Node.js examples.

---
+++ examples/node-js/simple-calendar.js
@@ -0,0 +1,20 @@
+var Calendar = require('../..').Calendar
+
+var calendar = new Calendar( { siblingMonths: false, weekStart: 1 } )
+var today = new Date()
+
+var table = [ ' Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su', '\n' ]
+
+calendar.getCalendar( today.getUTCFullYear(), today.getUTCMonth() ).forEach( function ( date ) {
+	if ( date ) {
+		table.push( ( date.day < 10 ? ' ' : '' ) + date.day )
+		if ( date.weekDay == 0 ) {
+			table.push( '\n' )
+		}
+	}
+	else {
+		table.push( '  ' )
+	}
+} )
+
+console.log( 'This month‘s calendar\n', table.join( '  ' ) )
