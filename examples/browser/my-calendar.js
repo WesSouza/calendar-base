8bdd3734a4f4c1c108e9d86f94d3131521194c15
Wesley de Souza <wesleywex@gmail.com>
2015-02-23 22:22:59 +0100

    Added browser example.

---
+++ examples/browser/my-calendar.js
@@ -0,0 +1,22 @@
+(function ( ) {
+	'use strict'
+
+	var months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ]
+
+	var calendar = new calendarBase.Calendar( { siblingMonths: true, weekStart: true } )
+	var today = new Date()
+	var $calendarMonth = document.querySelector( '.js-calendar-month' ),
+		$calendar = document.querySelector( '.js-calendar' )
+
+	$calendarMonth.innerHTML = months[ today.getUTCMonth() ]
+
+	calendar.getCalendar( today.getUTCFullYear(), today.getUTCMonth() ).forEach( function ( date ) {
+		var div = document.createElement( 'li' )
+		if ( date ) {
+			div.className = 'calendar-day'+ ( date.siblingMonth ? ' -sibling-month': '' )
+			div.innerHTML = date.day
+		}
+		$calendar.appendChild( div )
+	} )
+
+}() )
