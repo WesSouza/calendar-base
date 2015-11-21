f628fd4367090a891b13ebc397f6c452e76b5fc1
Wesley de Souza <wesleywex@gmail.com>
2015-11-21 12:25:09 +0100

    Removing spaces

--- examples/browser/my-calendar.js
+++ examples/browser/my-calendar.js
@@ -1,22 +1,22 @@
-(function ( ) {
+(function () {
 	'use strict'
 
-	var months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ]
+	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-	var calendar = new calendarBase.Calendar( { siblingMonths: true, weekStart: true } )
+	var calendar = new calendarBase.Calendar({ siblingMonths: true, weekStart: true })
 	var today = new Date()
-	var $calendarMonth = document.querySelector( '.js-calendar-month' ),
-		$calendar = document.querySelector( '.js-calendar' )
+	var $calendarMonth = document.querySelector('.js-calendar-month'),
+		$calendar = document.querySelector('.js-calendar')
 
-	$calendarMonth.innerHTML = months[ today.getUTCMonth() ]
+	$calendarMonth.innerHTML = months[today.getUTCMonth()]
 
-	calendar.getCalendar( today.getUTCFullYear(), today.getUTCMonth() ).forEach( function ( date ) {
-		var div = document.createElement( 'li' )
-		if ( date ) {
-			div.className = 'calendar-day'+ ( date.siblingMonth ? ' -sibling-month': '' )
+	calendar.getCalendar(today.getUTCFullYear(), today.getUTCMonth()).forEach(function (date) {
+		var div = document.createElement('li')
+		if (date) {
+			div.className = 'calendar-day'+ (date.siblingMonth ? ' -sibling-month': '')
 			div.innerHTML = date.day
 		}
-		$calendar.appendChild( div )
-	} )
+		$calendar.appendChild(div)
+	})
 
-}() )
+}())
