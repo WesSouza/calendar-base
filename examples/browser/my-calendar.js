0272268cd4ba8d3f2857a123f2af9b76340b0d49
Wes Souza <wesleydesouza@me.com>
2020-03-04 22:14:13 -0500

    Update examples

--- examples/browser/my-calendar.js
+++ examples/browser/my-calendar.js
@@ -1,22 +1,38 @@
-(function () {
-	'use strict'
+'use strict';
 
-	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
 
-	var calendar = new calendarBase.Calendar({ siblingMonths: true, weekStart: true })
-	var today = new Date()
-	var $calendarMonth = document.querySelector('.js-calendar-month'),
-		$calendar = document.querySelector('.js-calendar')
+const calendar = new CalendarBase.Calendar({
+  siblingMonths: true,
+  weekStart: true,
+});
+const today = new Date();
+const $calendarMonth = document.querySelector('.js-calendar-month'),
+  $calendar = document.querySelector('.js-calendar');
 
-	$calendarMonth.innerHTML = months[today.getUTCMonth()]
+$calendarMonth.innerHTML = months[today.getUTCMonth()];
 
-	calendar.getCalendar(today.getUTCFullYear(), today.getUTCMonth()).forEach(function (date) {
-		var div = document.createElement('li')
-		if (date) {
-			div.className = 'calendar-day'+ (date.siblingMonth ? ' -sibling-month': '')
-			div.innerHTML = date.day
-		}
-		$calendar.appendChild(div)
-	})
-
-}())
+calendar
+  .getCalendar(today.getUTCFullYear(), today.getUTCMonth())
+  .forEach(function(date) {
+    const div = document.createElement('li');
+    if (date) {
+      div.className =
+        'calendar-day' + (date.siblingMonth ? ' -sibling-month' : '');
+      div.innerHTML = date.day;
+    }
+    $calendar.appendChild(div);
+  });
